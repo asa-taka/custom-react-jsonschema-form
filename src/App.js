f2a2c2b628067c30139c7c7cb6edba07754a9a78
lbcat <lightbulbcat@icloud.com>
2018-01-21 18:54:07 +0900

    Add credits and README

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
 import React, { Component } from 'react';
 import Form from './Form'
 import './App.css';
-import { Icon, Button,  } from 'semantic-ui-react'
+import { Icon, Button, Segment, Container, Menu, Header } from 'semantic-ui-react'
 
 import schema from './schema'
 
 class App extends Component {
   render() {
     return <div className="App">
-      <header className="App-header">
-        <h1 className="App-title">Welcome to React</h1>
-      </header>
+      <Segment className="AppHeader" inverted vertical>
+        <Container>
+          <h1>Custamizing react-jsonschema-form for Semantic UI ♡</h1>
+        </Container>
+      </Segment>
+      <Segment vertical>
         <div className="ui container">
           <Form schema={schema}>
             <Button primary><Icon name="mail" />Submit</Button>
             <Button>Cancel</Button>
           </Form>
         </div>
+      </Segment>
+      <Segment vertical inverted>
+        <Container>
+          <h2>Powered by:</h2>
+          <ul>
+            <li><a href="https://github.com/mozilla-services/react-jsonschema-form">mozilla-services/react-jsonschema-form</a></li>
+            <li><a href="https://github.com/Semantic-Org/Semantic-UI/tree/1.0">Semantic-Org/Semantic-UI</a></li>
+          </ul>
+          <h3>Author</h3>
+          <a href="https://github.com/asa-taka/custom-react-jsonschema-form">asa-taka/custom-react-jsonschema-form</a>
+        </Container>
+      </Segment>
     </div>;
   }
 }
