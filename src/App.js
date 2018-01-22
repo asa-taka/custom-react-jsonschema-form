85d7630f08eb41e43bf016d5e2afe279f3eee941
asa-taka <lightbulbcat@icloud.com>
2018-01-22 16:34:10 +0900

    Fix Router to be HashRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { BrowserRouter as Router, Switch, Route, Link, withRouter } from 'react-router-dom'
+import { HashRouter as Router, Switch, Route, Link, withRouter } from 'react-router-dom'
 import Form from './Form'
 import './App.css'
 import {
